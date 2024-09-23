5c0a0fa4ed1c50bd41e9958b1bfad085c6b2336a
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-09-23 13:34:30 +0200

    Skapat divisions funktionen med try catch för division med noll.

--- Kalkylator_grupp/Program.cs
+++ Kalkylator_grupp/Program.cs
@@ -6,5 +6,18 @@
         {
             Console.WriteLine("Fractions");
         }
+
+        static double Divide(double a, double b)
+        {
+            try
+            {
+                double fraction = a / b;
+                return fraction;
+            }
+            catch (DivideByZeroException) 
+            {
+                Console.WriteLine("Invalid input. Division by zero is not authorized");
+            }
+        }
     }
 }
