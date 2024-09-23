11401186ba13839d3d707194dd85e83ae6d4392a
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2024-09-23 13:36:49 +0200

    Add funktion

--- Kalkylator_grupp/Program.cs
+++ Kalkylator_grupp/Program.cs
@@ -6,5 +6,10 @@
         {
             Console.WriteLine("Fractions");
         }
+
+        public static int Addition(int value1, int value2)
+        {
+            return value1 + value2;
+        }
     }
 }
