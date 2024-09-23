e5285f0bfe6242ebc3d8fb8f0edecdafff23e87e
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2024-09-23 13:20:52 +0200

    Add project files.

---
+++ Kalkylator_grupp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kalkylator_grupp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
